ec0bebee8d1309a9345db6155f3bacbcf110dc80
Rhodey Orbits <rhodey@anhonesteffort.org>
2017-12-22 14:00:36 -0500

    force

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.profile ~/.profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/dotfiles/.bash_exports ~/.bash_exports
+ln -s -f ~/dotfiles/.profile ~/.profile
+ln -s -f ~/dotfiles/.bashrc ~/.bashrc
+ln -s -f ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s -f ~/dotfiles/.bash_exports ~/.bash_exports
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.jshint ~/.jshint
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/.jshint ~/.jshint
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
